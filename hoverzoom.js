2cffccce7973ce837a98700650f620f08923a53d
wechselstift <wechselstift@gmail.com>
2026-01-07 11:56:19 +0100

    Update hoverzoom.js

--- hoverzoom.js
+++ hoverzoom.js
@@ -17,14 +17,21 @@ document.addEventListener("DOMContentLoaded", function () {
         });
 
     });
+    
+fetch("navbar.html")
+  .then(res => res.text())
+  .then(data => {
+    const nav = document.getElementById("navbar");
+    if (nav) nav.innerHTML = data;
+  });
 
-   fetch("navbar.html")
-    .then(res => res.text())
-    .then(data => document.getElementById("navbar").innerHTML = data);
-
-   fetch("footer.html")
-    .then(res => res.text())
-    .then(data => document.getElementById("footer").innerHTML = data);
+fetch("footer.html")
+  .then(res => res.text())
+  .then(data => {
+    const foot = document.getElementById("footer");
+    if (foot) foot.innerHTML = data;
+  });
     
 });
+
 
