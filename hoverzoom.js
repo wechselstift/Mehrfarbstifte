a031dc484c49292d667d2929d6d82f640b543776
Wechselstift <wechselstift@gmail.com>
2026-06-02 03:01:46 +0200

    waldmann

--- hoverzoom.js
+++ hoverzoom.js
@@ -35,24 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-    
-    var sideimages = document.querySelectorAll(".sideimage, .sideimage-long");
-    
-    sideimages.forEach(function (img) {
 
-        img.addEventListener("mouseenter", function () {
-            this.style.transform = "scale(1.8)";
-            this.style.transition = "transform 0.15s ease";
-            this.style.zIndex = "1000";
-            this.style.position = "relative";
-        });
-
-        img.addEventListener("mouseleave", function () {
-            this.style.transform = "scale(1)";
-            this.style.zIndex = "auto";
-        });
-
-          });
 
      fetch("rechts.html")
   .then(res => res.text())
