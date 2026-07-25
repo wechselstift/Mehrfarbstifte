52e37a71ef33d3e27d9178bc050aba40ab0cf4e4
unknown <wechselstift@gmail.com>
2026-07-25 23:28:08 +0200

    e

--- counter.js
+++ counter.js
@@ -3,14 +3,16 @@
 
 function initialisiereWebseite() {
 	
-
-      fetch("https://vierfarbkuli.goatcounter.com/counter/TOTAL")
+fetch("https://vierfarbkuli.goatcounter.com/counter/TOTAL.json")
     .then(response => response.json())
     .then(data => {
         let zahl = Number(data.count) + 1000;
 
         document.getElementById("counter").textContent =
             zahl.toLocaleString("de-DE");
+    })
+    .catch(error => {
+        console.error("Counter Fehler:", error);
     });
 }
 
