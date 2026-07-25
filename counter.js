ce027690c01fbade82fb48115b8936384082af03
unknown <wechselstift@gmail.com>
2026-07-25 23:31:18 +0200

    e

--- counter.js
+++ counter.js
@@ -6,10 +6,10 @@ function initialisiereWebseite() {
 fetch("https://vierfarbkuli.goatcounter.com/counter/TOTAL.json")
     .then(response => response.json())
     .then(data => {
-        let zahl = Number(data.count) + 1000;
+        let zahl = Number(data.count) + 82;
 
         document.getElementById("counter").textContent =
-            zahl.toLocaleString("de-DE");
+            "Besucher: " + zahl.toLocaleString("de-DE");
     })
     .catch(error => {
         console.error("Counter Fehler:", error);
